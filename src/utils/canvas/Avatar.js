d7b90540b80613c571ca66224b99e550247ff58c
Ivan Garcia <ivanvlora@gmail.com>
2019-09-13 18:32:40 +0900

    added promise image loading

--- src/utils/canvas/Avatar.js
+++ src/utils/canvas/Avatar.js
@@ -1,26 +1,33 @@
-function drawImages(ctx, clothes) {
-  const images = [];
-  let countLoaded = 0;
-  const clothesArray = Object.values(clothes).filter($0 =>
-    $0.startsWith('images')
-  );
-  const clothCount = clothesArray.length;
-  // Object.values(clothes).forEach((piece, idx) => {
-  clothesArray.forEach((piece, idx) => {
-    var img = new Image();
-    img.src = piece;
-    images.push(img);
+// const images = [];
+// let countLoaded = 0;
+// const clothesArray = Object.values(clothes).filter($0 =>
+//   $0.startsWith('images')
+// );
+// const clothCount = clothesArray.length;
+// // Object.values(clothes).forEach((piece, idx) => {
+// clothesArray.forEach((piece, idx) => {
+//   var img = new Image();
+//   img.src = piece;
+//   images.push(img);
 
-    img.onload = function() {
-      countLoaded++;
-      if (countLoaded !== clothCount) {
-        return;
-      }
-      for (var i = 0; i < clothesArray.length; i++) {
-        ctx.drawImage(images[i], 0, 0);
-      }
-      countLoaded = 0;
-    };
+//   img.onload = function() {
+//     countLoaded++;
+//     if (countLoaded !== clothCount) {
+//       return;
+//     }
+//     for (var i = 0; i < clothesArray.length; i++) {
+//       ctx.drawImage(images[i], 0, 0);
+//     }
+//     countLoaded = 0;
+//   };
+// });
+
+function preloadImage(source) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+
+    image.addEventListener('load', resolve.bind(null, image));
+    image.src = source;
   });
 }
 
@@ -41,6 +48,7 @@ class Avatar {
     this.canvas = canvas;
     this.ctx = ctx;
     this.imagesToRender = [];
+    this.previousClothes = null;
   }
 
   render(data) {
@@ -51,9 +59,23 @@ class Avatar {
     delete clothes.upperPose;
     delete clothes.lowerPose;
 
-    drawImages(this.ctx, clothes);
+    this.drawImages(clothes);
   }
 
+  drawImages(clothes) {
+    console.log(this.previousClothes);
+    console.log(clothes);
+    const clothesArray = Object.values(clothes).filter($0 =>
+      $0.startsWith('images')
+    );
+
+    Promise.all(clothesArray.map(preloadImage)).then(imgs => {
+      for (var i = 0; i < clothesArray.length; i++) {
+        this.ctx.drawImage(imgs[i], 0, 0);
+      }
+      this.previousClothes = clothes;
+    });
+  }
   // changeClothes(type, data) {
   //   if (type === 'hair') {
   //     drawImages(this.ctx, data);
