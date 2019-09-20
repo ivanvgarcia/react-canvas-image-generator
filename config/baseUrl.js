c6c1c1b21929a746af10d720b41c6e575fa4fdb3
Ivan Garcia <ivanvlora@gmail.com>
2019-09-20 18:01:52 +0900

    changed api url

--- config/baseUrl.js
+++ config/baseUrl.js
@@ -6,7 +6,7 @@ let baseURL;
 if (dev) {
   baseURL = 'http://localhost:5000';
 } else {
-  baseURL = 'https://d1riqzz03dhs7s.cloudfront.net';
+  baseURL = 'https://rliwdxmkx1.execute-api.us-east-1.amazonaws.com/prod';
 }
 
 export const avatarApi = axios.create({
