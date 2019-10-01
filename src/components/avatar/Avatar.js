3346f228cb29e678d8ce5f22a1a348e4f07a43ef
Ivan Garcia <ivanvlora@gmail.com>
2019-10-01 13:26:52 +0900

    test touch event

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -2,7 +2,6 @@ import React, { useRef, useEffect, useState } from 'react';
 import { Image } from 'react-konva';
 import { useSelector, useDispatch } from 'react-redux';
 import { reorderAvatars } from 'actions/avatar';
-import Hammer from 'hammerjs';
 
 const Avatar = ({ zIndex, chosenAvatar, selectedAvatar }) => {
   const avatarRef = useRef();
@@ -23,6 +22,11 @@ const Avatar = ({ zIndex, chosenAvatar, selectedAvatar }) => {
   //   step: 0
   // });
 
+  function getDistance(p1, p2) {
+    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+  }
+  var lastDist = 0;
+
   useEffect(() => {
     let image = new window.Image();
 
@@ -31,28 +35,6 @@ const Avatar = ({ zIndex, chosenAvatar, selectedAvatar }) => {
     };
     image.crossOrigin = '';
     image.src = chosenAvatar.url;
-
-    var hammertime = new Hammer(avatarRef.current, { domEvents: true });
-    hammertime.get('rotate').set({ enable: true });
-
-    console.log(hammertime);
-    var oldRotation = 0;
-    var startScale = 0;
-    avatarRef.current.on('rotatestart', function(ev) {
-      setCheck('listening...');
-      oldRotation = ev.evt.gesture.rotation;
-      startScale = avatarRef.current.attrs.scaleX();
-      avatarRef.current.stopDrag();
-      avatarRef.current.draggable(false);
-    });
-
-    avatarRef.current.on('rotate', function(ev) {
-      var delta = oldRotation - ev.evt.gesture.rotation;
-      avatarRef.current.rotate(-delta);
-      oldRotation = ev.evt.gesture.rotation;
-      avatarRef.current.scaleX(startScale * ev.evt.gesture.scale);
-      avatarRef.current.scaleY(startScale * ev.evt.gesture.scale);
-    });
   }, [chosenAvatar.url]);
 
   return (
@@ -83,6 +65,45 @@ const Avatar = ({ zIndex, chosenAvatar, selectedAvatar }) => {
           const node = avatarRef.current;
           node.zIndex(zIndex);
           selectedAvatar(avatarRef.current.attrs.name);
+        }}
+        onTouchStart={e => {
+          console.log('holla');
+        }}
+        onTouchMove={e => {
+          var touch1 = e.evt.touches[0];
+          var touch2 = e.evt.touches[1];
+          console.log(e.evt.touches);
+
+          console.log(touch1, '1');
+          console.log(touch2, '2');
+
+          if (touch1 && touch2) {
+            var dist = getDistance(
+              {
+                x: touch1.clientX,
+                y: touch1.clientY
+              },
+              {
+                x: touch2.clientX,
+                y: touch2.clientY
+              }
+            );
+
+            if (!lastDist) {
+              lastDist = dist;
+            }
+
+            var scale = (avatarRef.current.scaleX() * dist) / lastDist;
+
+            avatarRef.current.scaleX(scale);
+            avatarRef.current.scaleY(scale);
+            avatarRef.current.draw();
+            lastDist = dist;
+            setCheck(lastDist);
+          }
+        }}
+        onTouchEnd={e => {
+          console.log('peace');
         }}
         onTransformEnd={() => {
           const node = avatarRef.current;
