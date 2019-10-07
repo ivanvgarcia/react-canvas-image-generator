c976119346843d1603f2e17a66e3a7d30e265b99
Ivan Garcia <ivanvlora@gmail.com>
2019-10-07 12:34:21 +0900

    added Facebook sharing | removed packages that are not being used | added FB icon svg | styled FB button

--- src/components/confirmationScreen/ConfirmationScreen.js
+++ src/components/confirmationScreen/ConfirmationScreen.js
@@ -1,18 +1,43 @@
-import React, { useState } from 'react'
-import { Styles } from "./Styles"
+import React, { useState, useEffect } from 'react';
+import { Styles } from './Styles';
 import { TwitterShareButton } from 'react-twitter-embed';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { ReactComponent as Facebook } from 'components/svgs/facebook.svg';
 
 const ConfirmationScreen = ({ avatarImg, goNext, goBack, screen }) => {
   const [confirm, setConfirm] = useState(false);
 
-  const handleConfirm = (e) => {
-    const answer = e.currentTarget.textContent
-    if (answer === "No") {
-      return goBack(screen.previous)
+  const handleConfirm = e => {
+    const answer = e.currentTarget.textContent;
+    if (answer === 'No') {
+      return goBack(screen.previous);
     }
-    setConfirm(true)
-  }
+    setConfirm(true);
+  };
+
+  useEffect(() => {
+    window.fbAsyncInit = function() {
+      // eslint-disable-next-line no-undef
+      FB.init({
+        appId: '396269361288665',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v4.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = '//connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  }, []);
+
+  console.log(avatarImg);
 
   return (
     <Styles.Confirmation>
@@ -24,33 +49,61 @@ const ConfirmationScreen = ({ avatarImg, goNext, goBack, screen }) => {
               Are you happy with your creation?
             </Styles.ConfirmationText>
             <Styles.ConfirmationButtons>
-              <Styles.ConfirmationButton backgroundColor="green" onClick={handleConfirm}>
+              <Styles.ConfirmationButton
+                backgroundColor="green"
+                onClick={handleConfirm}
+              >
                 Yes
               </Styles.ConfirmationButton>
-              <Styles.ConfirmationButton backgroundColor="red" onClick={handleConfirm}>No</Styles.ConfirmationButton>
+              <Styles.ConfirmationButton
+                backgroundColor="red"
+                onClick={handleConfirm}
+              >
+                No
+              </Styles.ConfirmationButton>
             </Styles.ConfirmationButtons>
           </>
         ) : (
-            <>
-              <Styles.ConfirmationText>Your Avatar is Completed!</Styles.ConfirmationText>
+          <>
+            <Styles.ConfirmationText>
+              Your Avatar is Completed!
+            </Styles.ConfirmationText>
+
+            <Styles.ShareButtons>
+              <Styles.FaceBookShareButton
+                onClick={() => {
+                  // eslint-disable-next-line no-undef
+                  if (FB) {
+                    // eslint-disable-next-line no-undef
+                    FB.ui({
+                      method: 'share',
+                      href: 'https://d1riqzz03dhs7s.cloudfront.net/',
+                      hashtag: '#United',
+                      quote: 'Share your avatar'
+                    });
+                  }
+                }}
+              >
+                <Facebook></Facebook> Share
+              </Styles.FaceBookShareButton>
               <TwitterShareButton
-                url={"Image URL"}
+                url={'Image URL'}
                 options={{
                   text: `${'Avatar'}`,
                   via: 'United',
                   size: 'large'
                 }}
               />
-            </>
-          )}
+            </Styles.ShareButtons>
+          </>
+        )}
       </Styles.ConfirmationAlert>
-
     </Styles.Confirmation>
-  )
-}
+  );
+};
 
 ConfirmationScreen.propTypes = {
   avatarImg: PropTypes.string.isRequired
-}
+};
 
-export default ConfirmationScreen
+export default ConfirmationScreen;
--- src/components/confirmationScreen/Styles.js
+++ src/components/confirmationScreen/Styles.js
@@ -1,53 +1,61 @@
-
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Confirmation = styled.div`
-background-color: #ffffff;
-background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='0' %3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
+  background-color: #ffffff;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='0' %3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
 `;
 
 const ConfirmationAlert = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-position: fixed;
-bottom: 20px;
-left: 0;
-width: 90%;
-background: rgba(219, 71, 108, 0.5);
-border-radius: 20px;
-box-shadow: 0 2px 10px #4d4949;
-padding: 10px;
-margin: 0 10px;
-min-height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: fixed;
+  bottom: 20px;
+  left: 0;
+  width: 90%;
+  background: rgba(219, 71, 108, 0.5);
+  border-radius: 20px;
+  box-shadow: 0 2px 10px #4d4949;
+  padding: 10px;
+  margin: 0 10px;
+  min-height: 55px;
 `;
 
 const ConfirmationText = styled.p`
-font-size: 1rem;
-font-weight: bold;
-color: white;
+  font-size: 1rem;
+  font-weight: bold;
+  color: white;
 `;
 
 const ConfirmationButtons = styled.div`
-display: flex;
+  display: flex;
 `;
 
 const ConfirmationButton = styled.button`
-background: ${props => props.backgroundColor};
+  background: ${props => props.backgroundColor};
 `;
 
-// const TweetContainer = styled.div`
-//   min-width: 300px;
-//   flex: 1;
-//   @media (max-width: 650px) {
-//     position: absolute;
-//     bottom: 0px;
-//     left: 0;
-//     width: 100%;
-//     background: none;
-//   }
-// `;
+const ShareButtons = styled.div`
+  display: flex;
+`;
+
+const FaceBookShareButton = styled.button`
+  background: #3b5897;
+  color: white;
+  width: 76px;
+  font-weight: 500;
+  border-radius: 4px;
+  padding: 1px 10px 1px 9px;
+  margin-right: 5px;
+  font-size: 13px;
+  font-weight: 500;
+  display: flex;
+  height: 28px;
+  svg {
+    margin-right: 5px;
+  }
+`;
 
 const TwitterContent = styled.div`
   display: flex;
@@ -59,7 +67,6 @@ const TwitterContent = styled.div`
   box-shadow: 0 2px 8px rgba(0, 0, 0.1);
   background-color: #ffffff;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg stroke='%23CCC' stroke-width='0' %3E%3Crect fill='%23F5F5F5' x='-60' y='-60' width='110' height='240'/%3E%3C/g%3E%3C/svg%3E");
-  
 `;
 
 const SampleImage = styled.img`
@@ -73,6 +80,7 @@ export const Styles = {
   ConfirmationButtons,
   ConfirmationButton,
   TwitterContent,
+  ShareButtons,
+  FaceBookShareButton,
   SampleImage
-}
-
+};
